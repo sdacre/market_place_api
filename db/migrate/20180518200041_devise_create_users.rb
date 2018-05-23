76f433d6156df4bce454a54dcf804b4781368cd4
Stephanie <stephdacre@gmail.com>
2018-05-23 14:51:01 -0700

    updated application controller for CSRF attack controls

--- db/migrate/20180518200041_devise_create_users.rb
+++ db/migrate/20180518200041_devise_create_users.rb
@@ -19,7 +19,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
+      t.string   :last_sign_in_ipbundle exec rspec spec/models/user_spec.rb
 
       ## Confirmable
       # t.string   :confirmation_token
