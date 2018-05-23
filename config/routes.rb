73ab671e06ff5274ab2ff152e031f142b3f42131
Stephanie <stephdacre@gmail.com>
2018-05-23 15:59:48 -0700

    added user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ MarketPlaceApi::Application.routes.draw do
               constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
       resources :users, :only => [:show]
+			resources :users, :only => [:show, :create]
     end
   end
 end
+
+
