8d1f401b858ee5b939b961e071734a0ea085eb35
Stephanie <stephdacre@gmail.com>
2018-05-23 16:34:55 -0700

    add destroy action to users controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ MarketPlaceApi::Application.routes.draw do
               constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
       resources :users, :only => [:show]
-			resources :users, :only => [:show, :create, :update]
+			resources :users, :only => [:show, :create, :update, :destroy]
     end
   end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -93,4 +93,15 @@ describe Api::V1::UsersController do
       it { should respond_with 422 }
     end
   end
+
+  describe "DELETE #destroy" do
+  before(:each) do
+    @user = FactoryBot.create :user
+    delete :destroy, params: { id: @user.id }, format: :json
+  end
+
+  it { should respond_with 204 }
+
+end
+
 end
