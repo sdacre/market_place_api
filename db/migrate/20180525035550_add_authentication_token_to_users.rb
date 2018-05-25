9cd892000e9772d23ed132f8ecbb8ba0d3e9cd5a
Stephanie <stephdacre@gmail.com>
2018-05-24 21:01:21 -0700

    created authentication token to users migration

--- db/migrate/20180525035550_add_authentication_token_to_users.rb
+++ db/migrate/20180525035550_add_authentication_token_to_users.rb
@@ -1,4 +1,4 @@
-class AddAuthenticationTokenToUsers < ActiveRecord::Migration
+class AddAuthenticationTokenToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :auth_token, :string, default: ""
     add_index :users, :auth_token, unique: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe User, type: :model do
 	it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
 	it { should validate_confirmation_of(:password) }
 	it { should allow_value('example@domain.com').for(:email) }
+  # we test the user actually respond to this attribute
+  it { should respond_to(:auth_token) }
+  .
+  .
+  .
+  # we test the auth_token is unique
+  it { should validate_uniqueness_of(:auth_token)}
 end
